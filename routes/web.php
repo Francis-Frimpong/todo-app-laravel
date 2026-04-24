1d3a3de5eef311df0428a7958b1135b2ba1e0e37
Francis <brakay1136@gmail.com>
2026-04-24 09:39:33 -0800

    todo page displaying

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos', [TodoController::class, 'update']);
+Route::delete('/todos', [TodoController::class, 'destroy']);
